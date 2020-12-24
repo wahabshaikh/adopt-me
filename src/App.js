39dcf77d484c4f6237d3171b36392c41741a0c83
Wahab Shaikh <wahab.shaikh@somaiya.edu>
2020-12-24 20:01:34 +0530

    Learnt the use of JSX and adjusted its compatibility with ESLint.

--- src/App.js
+++ src/App.js
@@ -2,24 +2,14 @@ import React from "react";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Courage",
-      animal: "Dog",
-      breed: "Cowardly",
-    }),
-    React.createElement(Pet, {
-      name: "Oggy",
-      animal: "Cat",
-      breed: "Chartreux",
-    }),
-    React.createElement(Pet, {
-      name: "Mickey",
-      animal: "Mouse",
-      breed: "Cartoon",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Courage" animal="Dog" breed="Cowardly" />
+      <Pet name="Oggy" animal="Cat" breed="Chartreux" />
+      <Pet name="Mickey" animal="Mouse" breed="Cartoon" />
+    </div>
+  );
 };
 
 export default App;
